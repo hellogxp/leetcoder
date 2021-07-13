feb234ddce03cc1c2a1d5869d129c6b53b0ce1f9
燕衡 <xueping.gxp@alibaba-inc.com>
2021-07-13 09:21:42 +0800

    Done: reverse integer

---
+++ algorithms/src/reverse/integer/local/Solution.java
@@ -0,0 +1,30 @@
+package reverse.integer.local;
+
+/**
+ * @Description: TODO
+ * @Author chopin
+ * @Date 2021/7/13 09:01
+ * @Version 1.0
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(reverse(521));
+    }
+
+    public static int reverse(int x) {
+        int ans = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            // Determine weather the result overflows at every step, ans = ans * 10 + pop > MAX_VALUE
+            if (ans > Integer.MAX_VALUE / 10 || (ans == Integer.MAX_VALUE / 10 && pop > 7)) {
+                return 0;
+            }
+            if (ans < Integer.MIN_VALUE / 10 || (ans == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0;
+            }
+            ans = ans * 10 + pop;
+            x /= 10;
+        }
+        return ans;
+    }
+}
